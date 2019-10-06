c18952ca858b28d2866f901cf3a3b5c4d4bc2029
Mikhail <mikhailmcinally12@gmail.com>
2019-10-06 16:21:43 -0700

    start v2

---
+++ v2/js/main.js
@@ -0,0 +1,24 @@
+
+const masthead = document.querySelector('.inner-masthead');
+const title = document.querySelector('.inner-masthead h1');
+const subTitle = document.querySelector('.inner-masthead h5');
+const brand = document.querySelector('nav .logo');
+const h1 = document.querySelector('nav h1');
+console.log(h1);
+
+
+// h1.addEventListener('click', event => {
+//     brand.classList.add('highlight');
+// });
+
+
+// Logo mouse-enter event
+brand.addEventListener('click', event => {
+    //set color to purple
+    //reset the color after five seconds
+});
+
+// masthead mouse-enter event
+masthead.addEventListener('click', event => {
+    //add class to to-be-transitioned elements
+});
